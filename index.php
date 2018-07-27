044f423e79b9474f1bcfd84d7b1f7d78b554179c
Mwiaou <albafica.pisukesu@yahoo.fr>
2018-07-27 13:38:49 +0200

    Ajout du fichier index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Je suis un titre</h1>
+	<?php  
+		$newvar = "Discover github";
+
+		echo $newvar;
+	?>
+</body>
+</html>
